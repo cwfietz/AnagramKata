473d3d527f7360a441479061dae7ca270ee9018a
cwfietz <cwfietz@calcna.ab.ca>
2021-01-30 02:02:09 -0700

    Add Done to machine-setup.ps1

--- machine-setup.ps1
+++ machine-setup.ps1
@@ -1,11 +1,14 @@
 # How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/cwfietz/AnagramKata/cwfietz/AnagramKata/machine-setup.ps1 | iex
+#  PS> Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/cwfietz/AnagramKata/main/machine-setup.ps1 | Invoke-Expression
 
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | iex
+Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | Invoke-Expression
+Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/javascript-webstorm.ps1 | Invoke-Expression
 
 # Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/cwfietz/AnagramKata.git C:\Code\AnagramKata
 cd C:\Code\AnagramKata
 npm install 
 npm run test 
+
+# Done
+echo Done!
